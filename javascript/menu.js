afd4b1fb29dcf8c7cf717b3096f5ee51d6929ef1
lohezicgael <lohezicgael@gmail.com>
2024-11-18 20:55:44 +0100

    Correction bug menu burger

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,14 +1,23 @@
 function showResponsiveMenu() {
   var menu = document.getElementById("topnav_responsive_menu");
-  var icon = document.getElementById("topnav_hamburger_icon");
-  var root = document.getElementById("root");
-  if (menu.className === "") {
-    menu.className = "open";
-    icon.className = "open";
-    root.style.overflowY = "hidden";
+  var infos = document.querySelectorAll(".info"); // Sélectionne tous les éléments .info
+
+  // Vérifie si le menu est déjà ouvert
+  if (menu.classList.contains("open")) {
+      menu.classList.remove("open"); // Ferme le menu
+      menu.style.display = "none";
+
+      // Supprime la classe hidden des éléments .info
+      infos.forEach(function(info) {
+          info.classList.remove("hidden");
+      });
   } else {
-    menu.className = "";                    
-    icon.className = "";
-    root.style.overflowY = "";
+      menu.classList.add("open"); // Ouvre le menu
+      menu.style.display = "block";
+
+      // Ajoute la classe hidden aux éléments .info
+      infos.forEach(function(info) {
+          info.classList.add("hidden");
+      });
   }
 }
