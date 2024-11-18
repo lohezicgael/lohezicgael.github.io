aff8bcc223be23f12a11354f0550bd02261b2053
lohezicgael <lohezicgael@gmail.com>
2024-11-18 22:14:03 +0100

    BUG MENU BURGER

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,11 +1,14 @@
+
 function showResponsiveMenu() {
   var menu = document.getElementById("topnav_responsive_menu");
   var infos = document.querySelectorAll(".info"); // Sélectionne tous les éléments .info
+  var icon = document.getElementById("topnav_hamburger_icon");
 
   // Vérifie si le menu est déjà ouvert
   if (menu.classList.contains("open")) {
       menu.classList.remove("open"); // Ferme le menu
       menu.style.display = "none";
+      icon.className = "open";
 
       // Supprime la classe hidden des éléments .info
       infos.forEach(function(info) {
@@ -14,6 +17,8 @@ function showResponsiveMenu() {
   } else {
       menu.classList.add("open"); // Ouvre le menu
       menu.style.display = "block";
+      icon.className = "";
+
 
       // Ajoute la classe hidden aux éléments .info
       infos.forEach(function(info) {
